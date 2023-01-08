16d7829dac170dc7985475ba4602801b57f92193
SergePek <pekchiorckin@mail.ru>
2023-01-08 22:34:50 +0300

    dammy data

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,31 +1,39 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-const Post = () => {
+import { Users } from "../../dummyData"
+
+const Post = ({ post }) => {
+    console.log(post);
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
-                        <span className="postUserName">Safak Kosoglu</span>
-                        <span className="postDate">5 min ago</span>
+                        <img className="postProfileImg"
+                            src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
+                            alt=""
+                        />
+                        <span className="postUserName">
+                            {Users.filter((u) => u.id === post.userId)[0].username}
+                        </span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVertIcon />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! Its my first post</span>
-                    <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post.photo} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className="likeIcon" src="/assets/like.png" alt="" />
                         <img className="likeIcon" src="/assets/heart.png" alt="" />
-                        <span className="postLikeCounter">35 people like it</span>
+                        <span className="postLikeCounter">{post.like} people like it</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 comments</span>
+                        <span className="postCommentText">{post.comment} comments</span>
                     </div>
                 </div>
             </div>
--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -6,10 +6,121 @@ const Rightbar = () => {
             <div className="rightbarWrapper">
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="/assets/gift.png" alt="" />
-                <span className="birthdayText">
-                    <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+                    <span className="birthdayText">
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
                     </span>
                 </div>
+                <img src="/assets/ad.png" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li> <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpeg"
+                                alt=""
+                                className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUserName">Jonn Carter</span>
+                    </li>
+                </ul>
             </div>
         </div>
     );
